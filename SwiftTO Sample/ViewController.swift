dc51f5393692e4892a0eca7adbeb1e69fa26c796
m-alani <marwan@alani.ca>
2019-08-11 19:39:43 -0400

    Initial Commit

---
+++ SwiftTO Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftTO Sample
+//
+//  Created by Marwan Alani on 2019-08-11.
+//  Copyright © 2019 Marwan Alani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
